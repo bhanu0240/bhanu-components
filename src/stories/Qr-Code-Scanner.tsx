17cecff57550d3f2db6766434f5d3ff735c7d8a6
bhanu0240 <bhanureddy0240@gmail.com>
2024-11-05 23:08:09 +0530

    configured QR code component file setup

---
+++ src/stories/Qr-Code-Scanner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Qr-Code-Scanner.css";
+
+export interface QRCodeScanner {}
+
+const QRCodeScanner = () => {
+  return <div>QRCodeScanner</div>;
+};
+
+export default QRCodeScanner;
